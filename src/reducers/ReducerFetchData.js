ff992b80a136cee803db37c9ecc822dd876c039d
Tunde <ajiboye.tunde@gmail.com>
2018-10-03 22:53:00 +0100

    remove unneccesary code

--- src/reducers/ReducerFetchData.js
+++ src/reducers/ReducerFetchData.js
@@ -1,18 +1,5 @@
 import { FETCH_DATA }  from "../actions/type";
 
-// export default (state = {}, action) => {
-//   switch (action.type) {
-//     case FETCH_DATA:
-//       if (action.payload) {
-//         /////////////////////////////what comes from the request url//////
-//         console.log("action.payload",action.payload)
-//         return action.payload;
-//       }
-//       break;
-//     default:
-//       return state;
-//   }
-
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_DATA:
